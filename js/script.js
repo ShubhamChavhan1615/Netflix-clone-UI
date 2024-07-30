4e313ca336217e226be5c1abc41694b6fa74e1b1
Shubham Chavhan <shubhamchavhan908@gmail.com>
2024-07-30 18:05:47 +0530

    hello commit

--- js/script.js
+++ js/script.js
@@ -1,74 +1,90 @@
-let icon = document.querySelector(".icon")
-let icon2 = document.querySelector(".icon2")
-let icon3 = document.querySelector(".icon3")
-let icon4 = document.querySelector(".icon4")
-let icon5 = document.querySelector(".icon5")
-let ans1 = document.querySelector(".ans1");
-let ans2 = document.querySelector(".ans2");
-let ans3 = document.querySelector(".ans3");
-let ans4 = document.querySelector(".ans4");
-let ans5 = document.querySelector(".ans5");
-let faq = document.querySelector(".faq")
+// let icon = document.querySelector(".icon")
+// let icon2 = document.querySelector(".icon2")
+// let icon3 = document.querySelector(".icon3")
+// let icon4 = document.querySelector(".icon4")
+// let icon5 = document.querySelector(".icon5")
+// let ans1 = document.querySelector(".ans1");
+// let ans2 = document.querySelector(".ans2");
+// let ans3 = document.querySelector(".ans3");
+// let ans4 = document.querySelector(".ans4");
+// let ans5 = document.querySelector(".ans5");
+// let faq = document.querySelector(".faq")
 
-icon.addEventListener("mouseover", () => {
-    // alert("icon cliked")
-    faq.style.height = "710px";
-    ans1.style.display = "block";
-})
+// icon.addEventListener("mouseover", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "710px";
+//     ans1.style.display = "block";
+// })
 
-icon.addEventListener("mouseout", () => {
-    // alert("icon cliked")
-    faq.style.height = "600px";
-    ans1.style.display = "none";
-})
+// icon.addEventListener("mouseout", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "600px";
+//     ans1.style.display = "none";
+// })
 
-icon2.addEventListener("mouseover", () => {
-    // alert("icon cliked")
-    faq.style.height = "710px";
-    ans2.style.display = "block";
-})
+// icon2.addEventListener("mouseover", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "710px";
+//     ans2.style.display = "block";
+// })
 
-icon2.addEventListener("mouseout", () => {
-    // alert("icon cliked")
-    faq.style.height = "600px";
-    ans2.style.display = "none";
-})
-
-
-icon3.addEventListener("mouseover", () => {
-    // alert("icon cliked")
-    faq.style.height = "710px";
-    ans3.style.display = "block";
-})
-
-icon3.addEventListener("mouseout", () => {
-    // alert("icon cliked")
-    faq.style.height = "600px";
-    ans3.style.display = "none";
-})
+// icon2.addEventListener("mouseout", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "600px";
+//     ans2.style.display = "none";
+// })
 
 
-icon4.addEventListener("mouseover", () => {
-    // alert("icon cliked")
-    faq.style.height = "710px";
-    ans4.style.display = "block";
-})
+// icon3.addEventListener("mouseover", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "710px";
+//     ans3.style.display = "block";
+// })
 
-icon4.addEventListener("mouseout", () => {
-    // alert("icon cliked")
-    faq.style.height = "600px";
-    ans4.style.display = "none";
-})
+// icon3.addEventListener("mouseout", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "600px";
+//     ans3.style.display = "none";
+// })
 
 
-icon5.addEventListener("mouseover", () => {
-    // alert("icon cliked")
-    faq.style.height = "710px";
-    ans5.style.display = "block";
-})
+// icon4.addEventListener("mouseover", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "710px";
+//     ans4.style.display = "block";
+// })
 
-icon5.addEventListener("mouseout", () => {
-    // alert("icon cliked")
-    faq.style.height = "600px";
-    ans5.style.display = "none";
-})
+// icon4.addEventListener("mouseout", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "600px";
+//     ans4.style.display = "none";
+// })
+
+
+// icon5.addEventListener("mouseover", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "710px";
+//     ans5.style.display = "block";
+// })
+
+// icon5.addEventListener("mouseout", () => {
+//     // alert("icon cliked")
+//     faq.style.height = "600px";
+//     ans5.style.display = "none";
+// })
+const icons = document.querySelectorAll(".icon");
+const faq = document.querySelector(".faq");
+
+icons.forEach(icon => {
+    icon.addEventListener("mouseover", () => {
+        const answer = document.querySelector(`.${icon.getAttribute("data-answer")}`);
+        faq.style.height = "710px";
+        answer.style.display = "block";
+    });
+
+    icon.addEventListener("mouseout", () => {
+        const answer = document.querySelector(`.${icon.getAttribute("data-answer")}`);
+        faq.style.height = "600px";
+        answer.style.display = "none";
+    });
+});
